2160a63efbfba30755caf78cf742b0c699dc1470
ethanw <ethansfwoo@gmail.com>
2024-02-17 15:43:00 -0700

    Some empty cases handled

--- src/logic/Goal.ts
+++ src/logic/Goal.ts
@@ -5,6 +5,15 @@ export interface Goal {
 }
 
 export function appendGoal(goal: Goal) {
+    if (goal.name === '')
+        goal.name = 'Unnamed goal';
+    if (goal.body === '')
+        goal.body = 'No description';
+    if (goal.amount === null)
+        goal.amount = 0;
+    else if (goal.amount < 0)
+        goal.amount = 0;
+
     const currentGoalsJson = (localStorage.getItem('goals'));
     const newGoalsArr = currentGoalsJson ? [...JSON.parse(currentGoalsJson), goal] : [goal];
     localStorage.setItem('goals', JSON.stringify(newGoalsArr));
@@ -27,6 +36,15 @@ export function deleteGoal(index: number) {
 }
 
 export function editGoal(goal: Goal, index: number) {
+    if (!goal.name)
+        goal.name = 'Unnamed goal';
+    if (!goal.body)
+        goal.body = 'No description';
+    if (!goal.amount)
+        goal.amount = 0;
+    else if (goal.amount < 0)
+        goal.amount = 0;
+    
     const currentGoalsJson = (localStorage.getItem('goals'));
     if (!currentGoalsJson)
         return;
