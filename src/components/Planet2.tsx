0e9142c487efdab2470a6f9a09098e332a157199
Faraz Hosseinian-Tehrani <107619942+farazht@users.noreply.github.com>
2024-02-18 10:23:32 -0700

    Create Planet2.tsx

---
+++ src/components/Planet2.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Planet2 = () => {
+
+    let completed = JSON.parse(localStorage.getItem('completed') || '0');
+
+    return (
+        <div className='relative'>
+            <img className='w-40 h-24' src='src/assets/planet2.png' alt='Planet2'></img>
+            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center'>
+                <p className='text-base font-bold'>{completed} goals completed!</p>
+            </div>
+        </div>
+    );
+};
+
+export default Planet2;
